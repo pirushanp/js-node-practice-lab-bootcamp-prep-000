5ad8114098cc15e555bb7566e29235c87d1b3a1a
pirushanp <pirushanp@gmail.com>
2017-10-15 22:24:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Pirushan"
-var height = "79"
+var height = 79
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
